1e15d947a9e852cfc3473367422957420dd3c87f
Merfik933 <anatoliy9370@gmail.com>
2025-06-28 12:46:20 +0300

    Add country selection

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from selenium import webdriver
 import pandas as pd
+import time
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 
 website = 'https://www.adamchoi.co.uk/overs/detailed'
 
 driver = webdriver.Chrome()
 
-
 driver.get(website)
 
 all_matches_button = driver.find_element(by='xpath', value='//label[@analytics-event="All matches"]')
 all_matches_button.click()
+
+dropdown = Select(driver.find_element(by='id', value='country'))
+dropdown.select_by_visible_text('Ukraine')
+
+time.sleep(3)
 
 date = [elem.text for elem in driver.find_elements(by='xpath', value='//tr/td[1]')]
 home_team = [elem.text for elem in driver.find_elements(by='xpath', value='//tr/td[3]')]
