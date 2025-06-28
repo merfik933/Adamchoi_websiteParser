3b6330c3978e457aa82d45480cc7ac111e2a48ee
Merfik933 <anatoliy9370@gmail.com>
2025-06-28 12:33:10 +0300

    Add base scraing functionality

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from selenium import webdriver
+import pandas as pd
 
-website = "https://www.adamchoi.co.uk/overs/detailed"
+website = 'https://www.adamchoi.co.uk/overs/detailed'
 
-path = "D:\Programs\Drivers\chrome-driver"
-driver = webdriver.Chrome(path)
+driver = webdriver.Chrome()
+
 
 driver.get(website)
 
+all_matches_button = driver.find_element(by='xpath', value='//label[@analytics-event="All matches"]')
+all_matches_button.click()
+
+date = [elem.text for elem in driver.find_elements(by='xpath', value='//tr/td[1]')]
+home_team = [elem.text for elem in driver.find_elements(by='xpath', value='//tr/td[3]')]
+score = [elem.text for elem in driver.find_elements(by='xpath', value='//tr/td[4]')]
+away_team = [elem.text for elem in driver.find_elements(by='xpath', value='//tr/td[5]')]
+
 driver.quit()
+
+df = pd.DataFrame({
+    'date': date,
+    'home_team': home_team,
+    'score': score,
+    'away_team': away_team
+})
+df.to_csv('football_data.csv', index=False)
